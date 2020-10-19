bbaa34f6a35f6c6f4517ca61b07283d7359cb39b
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-10-20 00:26:28 +0300

    Create sum_app_using_list.dart
    App helps you calculate sum of the list.
    Add or remove integer from list.
    show sum or reset list and sum.

---
+++ sum_app_using_list.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+//----------------------------------------------------
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Sum app'),
+    );
+  }
+}
+//----------------------------------------------------
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int _billAmount = 0;
+  var sumList = [];
+  int sumInt = 0;
+  //int _billAmount2 = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      //_counter= _counter + _billAmount;// + _billAmount2;
+      sumList.forEach((element) {
+        sumInt += element;
+      });
+      print("$sumInt Hi this is sumInt");
+    });
+  }
+
+  void _removeFromList() {
+    setState(() {
+      sumList.removeLast();
+    });
+  }
+
+  void _ZERO() {
+    setState(() {
+      //_counter= 0;
+      sumInt = 0;
+      sumList = [];
+    });
+  }
+
+  void _addToList() {
+    setState(() {
+      sumList.add(_billAmount);
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column( // can i get it as row?
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // InkWell(
+            //   // onTap: () {
+            //   //   setState(() {
+            //   //     _incrementCounter;
+            //   //   });
+            //   onTap: () {
+            //     // ignore: unnecessary_statements
+            //     _ZERO();
+            //   },
+            //   child: Container(
+            //     width: 200.0,
+            //     height: 70.0,
+            //     margin: EdgeInsets.all(10.0),
+            //     decoration: BoxDecoration(
+            //         color: Colors.brown.withOpacity(0.3),
+            //         borderRadius: BorderRadius.circular(7.0)),
+            //     child: Center(
+            //       child: Text(
+            //         "0",
+            //         style: TextStyle(
+            //             color: Colors.deepOrange,
+            //             fontSize: 60.0,
+            //             fontWeight: FontWeight.bold),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Text("$sumList", // The LIST in app!
+            style: TextStyle(
+              fontSize: 30,
+            ),),
+            //SizedBox(height: 10),
+            TextField(
+              style: TextStyle(
+                fontSize: 40,
+              ),
+              keyboardType:
+              TextInputType.numberWithOptions(decimal: true),
+              onChanged: (String value) {
+                try {
+                  _billAmount = int.parse(value);
+                } catch (exception) {
+                  _billAmount = 0;
+                }
+              },
+            ),
+//----------------------------------------------------------------------------
+          Center(
+            child: Row(children: [
+              InkWell(
+                // onTap: () {
+                //   setState(() {
+                //     _incrementCounter;
+                //   });
+                onTap: () {
+                  // ignore: unnecessary_statements
+                  _addToList();
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 40.0,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      color: Colors.brown.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(7.0)),
+                  child: Center(
+                    child: Text(
+                      "add!",
+                      style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              //--------2nd button--------------
+              InkWell(
+                // onTap: () {
+                //   setState(() {
+                //     _incrementCounter;
+                //   });
+                onTap: () {
+                  // ignore: unnecessary_statements
+                  _removeFromList();
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 40.0,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      color: Colors.brown.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(7.0)),
+                  child: Center(
+                    child: Text(
+                      "remove!",
+                      style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],),
+          ),
+//-----------------------------------------------------------------------------
+            SizedBox(height: 1),
+//--------------------------------------------------------------
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    // ignore: unnecessary_statements
+                    _incrementCounter();
+                  },
+                  child: Container(
+                    width: 200.0,
+                    height: 40.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.brown.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(7.0)),
+                    child: Center(
+                      child: Text(
+                        "sum of list",
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    // ignore: unnecessary_statements
+                    //_minusCounter();
+                    _ZERO();
+                  },
+                  child: Container(
+                    width: 150.0,
+                    height: 40.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.brown.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(7.0)),
+                    child: Center(
+                      child: Text(
+                        "sum=0",
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 1),
+//-----------------------THe sum text-------------------------------------------
+            Text(
+              'The sum:',
+              style: TextStyle(
+                //color: _purple,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '$sumInt',
+              style: TextStyle(
+                //color: _purple,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
