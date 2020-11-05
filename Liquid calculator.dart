5ab2fa9589bc3fd72d6e43cbce091aab9010c1f9
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-11-05 21:39:36 +0200

    Update Liquid calculator.dart

--- Liquid calculator.dart
+++ Liquid calculator.dart
@@ -51,40 +51,45 @@ class _TextInputPlaceState extends State<TextInputPlace> {
   double theEndResult = 0;
   List theList = [];
   List showEndList = [];
+
   //-------------------------------
-  double V =0.0;
-  double P=0.0;
+  double V = 0.0;
+  double P = 0.0;
+
 //-----------------------------------------------
-  showResult(List Y){
+  String showString = "Liquids: ";
+  int C = 0;
+
+  //----------------------------------------
+  showResult(List Y) {
     var L = calculateResult(Y);
     showEndList = L;
     print(showEndList);
     //double.parse((12.3412).toStringAsFixed(2));
     V = double.parse((showEndList[0]).toStringAsFixed(2));
     P = double.parse((showEndList[1]).toStringAsFixed(2));
-    return [V,P];
+    return [V, P];
   }
 
-  calculateResult(List X){
+  calculateResult(List X) {
     var endVolume = 0.0;
-    var endPersentage =0.0;
+    var endPersentage = 0.0;
     var pureLiquid = 0.0;
     List endResultList = [];
     //var X = [[1,50],[0.5,5]];
 //   X.add([1,80]);
 //   X.add([0.75,13]);
-    for(int i=0;i<X.length;i++){
+    for (int i = 0; i < X.length; i++) {
       var subList = X[i];
       var vol = subList[0];
       var proc = subList[1];
       print("int loop = $vol, $proc, $endVolume, $pureLiquid");
       endVolume += vol;
-      pureLiquid += vol * (proc/100);
+      pureLiquid += vol * (proc / 100);
       //endPersentage =
-
-    }
-    endPersentage = (pureLiquid/endVolume)*100;
-    endResultList = [endVolume,endPersentage];
+    } // for loop
+    endPersentage = (pureLiquid / endVolume) * 100;
+    endResultList = [endVolume, endPersentage];
     print("$endVolume, $endPersentage");
     // doubleVolToList = 0.0;
     // doublePercentToList = 0.0;
@@ -97,72 +102,93 @@ class _TextInputPlaceState extends State<TextInputPlace> {
     return Center(
       child: Column(
         children: [
-          Row(
-            children: [
-              Text(
-                'result:  ',
-                style: TextStyle(
-                  //color: _purple,
-                    fontSize: fontSizeLetters,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Volume = $V, Percentage = $P',
-                style: TextStyle(
-                  //color: _purple,
-                    fontSize: fontSizeLetters,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
+          Text(
+            'result:  ',
+            style: TextStyle(
+                //color: _purple,
+                fontSize: fontSizeLetters,
+                fontWeight: FontWeight.bold),
           ),
-          Text("$theList", // The LIST in app!
+          RichText(
+            text: TextSpan(
+              text:'Volume: ',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: fontSizeLetters,
+                  fontWeight: FontWeight.bold),
+            children: <TextSpan>[TextSpan(text: '$V', style: TextStyle(
+              color: Colors.teal,
+                fontSize: fontSizeLetters,
+                fontWeight: FontWeight.bold))],),
+          ),
+          RichText(
+            text: TextSpan(
+              text:'Percentage: ',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: fontSizeLetters,
+                  fontWeight: FontWeight.bold),
+              children: <TextSpan>[TextSpan(text: '$P', style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: fontSizeLetters,
+                  fontWeight: FontWeight.bold))],),
+          ),
+          Container(
+            width: double.infinity,
+            height: 10,
+            color: Colors.blue,
+          ),
+          Text(
+            "$showString", // The LIST in app! $theList
             style: TextStyle(
               fontSize: fontSizeLetters,
-            ),),
+            ),
+          ),
           //Text("$testString"),
           new Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               new Flexible(
-                child: new TextField(
-                  decoration: InputDecoration(
-                      hintText: "Enter volume"
-                  ),
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-
-                  onChanged: (String value) {
-                    try {
-                      doubleVolToList = double.parse(value);
-                    } catch (exception) {
-                      doubleVolToList = 0;
-                    }
-                  },
-                )
+                  child: new TextField(
+                decoration: InputDecoration(hintText: "Volume"),
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onChanged: (String value) {
+                  try {
+                    doubleVolToList = double.parse(value);
+                  } catch (exception) {
+                    doubleVolToList = 0;
+                  }
+                },
+              )),
+              SizedBox(
+                width: 20.0,
               ),
-              SizedBox(width: 20.0,),
               new Flexible(
                 child: new TextField(
-                  decoration: InputDecoration(
-                      hintText: "Enter percentage"
-                  ),
+                  decoration: InputDecoration(hintText: "Percentage"),
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 30,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold
                   ),
                   keyboardType: TextInputType.numberWithOptions(decimal: true),
-
                   onChanged: (String value) {
                     try {
-                      doublePercentToList= double.parse(value);
+                      doublePercentToList = double.parse(value);
                     } catch (exception) {
-                      doublePercentToList= 0;
+                      doublePercentToList = 0;
                     }
                   },
                 ),
               ),
-              SizedBox(width: 20.0,)
+              SizedBox(
+                width: 20.0,
+              )
             ],
           ),
           InkWell(
@@ -175,8 +201,10 @@ class _TextInputPlaceState extends State<TextInputPlace> {
               _addToList();
             },
             child: Container(
-              width: buttonWight, // buttonWight
-              height: buttonHeight, // buttonHeight
+              width: buttonWight,
+              // buttonWight
+              height: buttonHeight,
+              // buttonHeight
               margin: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
                   color: Colors.brown.withOpacity(0.3),
@@ -195,11 +223,37 @@ class _TextInputPlaceState extends State<TextInputPlace> {
           InkWell(
             onTap: () {
               // ignore: unnecessary_statements
+              //_incrementCounter();
+              _showResultFromList();
+            },
+            child: Container(
+              width: 180,
+              // buttonWight
+              height: buttonHeight,
+              // buttonHeight
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.brown.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(7.0)),
+              child: Center(
+                child: Text(
+                  "Result",
+                  style: TextStyle(
+                      color: bColor,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              // ignore: unnecessary_statements
               //_minusCounter();
               _ZERO();
             },
             child: Container(
-              width: 180.0,
+              width: 120.0,
               height: buttonHeight,
               margin: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -216,41 +270,16 @@ class _TextInputPlaceState extends State<TextInputPlace> {
               ),
             ),
           ),
-          InkWell(
-            onTap: () {
-              // ignore: unnecessary_statements
-              //_incrementCounter();
-              _showResultFromList();
-            },
-            child: Container(
-              width: buttonWight, // buttonWight
-              height: buttonHeight, // buttonHeight
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.brown.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(7.0)),
-              child: Center(
-                child: Text(
-                  "Result",
-                  style: TextStyle(
-                      color: bColor,
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-          ),
         ],
       ),
     );
   }
+
   void _addToList() {
     setState(() {
-      //double.parse((12.3412).toStringAsFixed(2)); doublePercentToList
-      theList.add([doubleVolToList,doublePercentToList]);
-      //theList.add([double.parse((doubleVolToList).toStringAsFixed(2)),
-      //double.parse((doublePercentToList).toStringAsFixed(2))]);
-
+      theList.add([doubleVolToList, (doublePercentToList>=100)?100:doublePercentToList]);
+      C++;
+      showString += "V$C=$doubleVolToList, P$C=${(doublePercentToList>=100)?100:doublePercentToList} & \n";
     });
   }
 
@@ -261,6 +290,10 @@ class _TextInputPlaceState extends State<TextInputPlace> {
       // doublePercentToList = 0.0;
       theList = [];
       showEndList = [];
+      showString = "Liquids: ";
+      V = 0.0;
+      P = 0.0;
+      C = 0;
     });
   }
 
@@ -270,8 +303,9 @@ class _TextInputPlaceState extends State<TextInputPlace> {
       //end_multiple;
     });
   }
-  double roundDouble(double value, int places){ 
-   double mod = pow(10.0, places); 
-  return ((value * mod).round().toDouble() / mod); 
+
+  double roundDouble(double value, int places) {
+    double mod = pow(10.0, places);
+    return ((value * mod).round().toDouble() / mod);
   }
 } //class _TextInputPlaceState
